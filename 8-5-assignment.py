74367ad7fa25ff46ac27396fe4c9993538af9e3c
declanmmeaney <66974581+declanmmeaney@users.noreply.github.com>
2020-06-15 17:08:21 -0400

    Add files via upload

---
+++ 8-5-assignment.py
@@ -0,0 +1,15 @@
+#8-5-assignment
+
+count = 0
+
+x = open('mbox-short.txt')
+for line in x:
+    line = line.rstrip()
+    if not line.startswith('From '): continue
+    parts = line.split()
+    email = parts[1]
+    count = count + 1
+
+
+    print(email)
+print("There were", count, "lines in the file with From as the first word")
